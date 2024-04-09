9b8e6bf3129fcfd551287b78c53b740009bf2fb0
Nazim <nazim86@gmail.com>
2024-04-09 10:46:03 +0400

    swagger configuration for vercel

--- src/core/config/swagger/swaggerSetup.ts
+++ src/core/config/swagger/swaggerSetup.ts
@@ -1,5 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 export function swaggerSetup(app: INestApplication) {
   const config = new DocumentBuilder()
@@ -13,4 +15,18 @@ export function swaggerSetup(app: INestApplication) {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(`swagger`, app, document);
+
+  // get the swagger json file (if app is running in development mode)
+  if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.json',
+    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
+  }
 }
