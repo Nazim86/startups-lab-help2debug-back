cc2b37b34cc87ae88cf5ab99a1e4c8cb5a97f4a8
Nazim <nazim86@gmail.com>
2024-04-10 12:02:12 +0400

    similar hashtag saving problem solved

--- src/feature/issue/application/use-cases/createIssue.usecase.ts
+++ src/feature/issue/application/use-cases/createIssue.usecase.ts
@@ -1,12 +1,12 @@
 import { CreateIssueDto } from '../../dto/createIssue.dto';
 import { UserFacade } from '../../../user/user.facade';
 import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { CreateHashtagCommand } from '../../../hashtag/application/use-cases/createHashtag.usecase';
 import { HashtagFacade } from '../../../hashtag/hashtag.facade';
 import { Hashtag } from '../../../hashtag/entities/hashtag.entity';
 import { IssueRepository } from '../../db/issue.repository';
 import { Result } from '../../../../core/result';
 import { Issue } from '../../entities/issue.entity';
+import { CreateHashtagDto } from '../../../hashtag/dto/createHashtag.dto';
 
 export class CreateIssueCommand {
   constructor(
@@ -41,56 +41,68 @@ export class CreateIssueUseCase implements ICommandHandler<CreateIssueCommand> {
     const foundHashtags: Hashtag[] =
       await this.hashtagFacade.repository.findHashtags(normalizedHashtags);
 
-    let hashtags: Hashtag[] = foundHashtags;
+    let hashtags: Hashtag[];
 
     //TODO: cover with transaction
 
     //TODO: DO I need update (push new) hashtag title if existing hashtag title does not match with user entered? If yes
     // then I need to check each title oneByOne and push the ones does not exist
 
-    if (hashtags.length && hashtags.length !== normalizedHashtags.length) {
+    if (
+      foundHashtags.length &&
+      foundHashtags.length !== normalizedHashtags.length
+    ) {
       const existingNormalizedHashtags = foundHashtags.map(
         (hashtag) => hashtag.normalized,
       );
-      console.log('existingNormalizedHashtags', existingNormalizedHashtags);
+      //console.log('existingNormalizedHashtags', existingNormalizedHashtags);
 
       const missingNormalizedHashtags = normalizedHashtags.filter(
         (normalizedHashtag) =>
           !existingNormalizedHashtags.includes(normalizedHashtag),
       );
 
-      console.log('missingNormalizedHashtags', missingNormalizedHashtags);
+      // console.log('missingNormalizedHashtags', missingNormalizedHashtags);
 
       const missingHashtagDto = mappedHashtagsDto.filter((hashtagDto) =>
         missingNormalizedHashtags.includes(hashtagDto.normalized),
       );
 
-      const createdHashtags = await this.commandBus.execute(
-        new CreateHashtagCommand(missingHashtagDto),
-      );
-      console.log('createdHashtags', hashtags);
-
-      hashtags = createdHashtags.value;
-
-      hashtags.push(...foundHashtags);
-
-      console.log('hashtag after push', hashtags);
+      hashtags = this.createHashtags(missingHashtagDto);
     }
 
-    if (!hashtags.length) {
-      hashtags = await this.commandBus.execute(
-        new CreateHashtagCommand(mappedHashtagsDto),
-      );
+    if (!foundHashtags.length) {
+      hashtags = this.createHashtags(mappedHashtagsDto);
     }
+
+    hashtags = await this.hashtagFacade.repository.saveHashtags(hashtags);
+
+    //joining found and missed hashtags
+    foundHashtags.push(...hashtags);
 
     const newIssue = new Issue();
     newIssue.type = createIssueDto.type;
     newIssue.title = createIssueDto.title;
     newIssue.description = createIssueDto.description;
     newIssue.user = user;
-    newIssue.hashtag = hashtags;
+    newIssue.hashtag = foundHashtags;
 
     const created = await this.issueRepo.saveIssue(newIssue);
     return Result.Ok(created);
+  }
+  private createHashtags(createHashtagDto: CreateHashtagDto[]) {
+    // Remove duplicates. If user entered same tag at least twice
+    const uniqueHashtags = createHashtagDto.filter((obj, index, array) => {
+      return (
+        index === array.findIndex((item) => item.normalized === obj.normalized)
+      );
+    });
+
+    return uniqueHashtags.map((hashtagDto) => {
+      const hashtag = new Hashtag();
+      hashtag.normalized = hashtagDto.normalized;
+      hashtag.title = hashtagDto.title;
+      return hashtag;
+    });
   }
 }
